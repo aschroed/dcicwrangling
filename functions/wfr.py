3d33d50ee7d0c82c17691d8b1a08407a8faab170
Koray Kırlı <koray_kirli@hms.harvard.edu>
2018-08-07 14:53:29 -0400

    update input json parameters

--- functions/wfr.py
+++ functions/wfr.py
@@ -113,7 +113,6 @@ def run_json(input_files, env, wf_info, run_name, tag):
                              "json_bucket": "4dn-aws-pipeline-run-json",
                              "ebs_iops": 500,
                              "shutdown_min": 30,
-                             "s3_access_arn": "arn:aws:iam::643366669028:instance-profile/S3_access",
                              "copy_to_s3": True,
                              "launch_instance": True,
                              "password": "dragonfly",
