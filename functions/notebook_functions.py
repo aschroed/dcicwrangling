11e487996b0ab132571dd56292ca358c7417af1d
Sarah <sarah_reiff@hms.harvard.edu>
2018-09-24 13:50:31 -0400

    Added a couple lines for ~ in file path

--- functions/notebook_functions.py
+++ functions/notebook_functions.py
@@ -17,6 +17,8 @@ def get_key(keyname=None, keyfile='keypairs.json'):
     if keyfile != 'keypairs.json':
         if not keyname:
             raise Exception('please provide keyname')
+        if keyfile.startswith('~'):
+            keyfile = os.path.expanduser("~") + keyfile[1:]
         with open(keyfile, 'r') as key_file:
             keys = key_file.read()
         my_key = json.loads(keys)[keyname]
