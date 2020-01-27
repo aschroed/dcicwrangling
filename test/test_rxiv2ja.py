2a97875c7971e27b7e6818ef823649337eb5478c
Sarah <sarah_reiff@hms.harvard.edu>
2020-01-27 10:01:52 -0500

    edited mocker.patch lines in test_rxiv2ja.py

--- test/test_rxiv2ja.py
+++ test/test_rxiv2ja.py
@@ -55,33 +55,27 @@ def test_remove_skipped_vals_w_good_atids(mocker):
     side_effect = ['uuid1', 'uuid2', 'uuid1', 'uuid2', 'uuid3']
     val = ['id1', 'id2']
     vals2skip = ['id1', 'id2', 'id3']
-    # import pdb; pdb.set_trace()
-    with mocker.patch('functions.script_utils.get_item_uuid',
-                      side_effect=side_effect):
-        result = rj.remove_skipped_vals(val, vals2skip)
-        assert not result
+    mocker.patch('functions.script_utils.get_item_uuid', side_effect=side_effect)
+    result = rj.remove_skipped_vals(val, vals2skip)
+    assert not result
 
 
 def test_remove_skipped_vals_w_item_lookup(mocker):
     side_effect = ['uuid1', 'uuid2', 'uuid1']
     val = ['id1', 'id2']
     vals2skip = ['id1']
-    # import pdb; pdb.set_trace()
-    with mocker.patch('functions.script_utils.get_item_uuid',
-                      side_effect=side_effect):
-        result = rj.remove_skipped_vals(val, vals2skip)
-        assert result[0] == val[1]
+    mocker.patch('functions.script_utils.get_item_uuid', side_effect=side_effect):
+    result = rj.remove_skipped_vals(val, vals2skip)
+    assert result[0] == val[1]
 
 
 def test_remove_skipped_vals_w_item_lookup_and_not_found(mocker):
     side_effect = ['uuid1', None, 'uuid3', 'uuid1', None]
     val = ['id1', 'id2', 'id3']
     vals2skip = ['id3', 'id4']
-    # import pdb; pdb.set_trace()
-    with mocker.patch('functions.script_utils.get_item_uuid',
-                      side_effect=side_effect):
-        result = rj.remove_skipped_vals(val, vals2skip)
-        assert result[0] == val[0]
+    mocker.patch('functions.script_utils.get_item_uuid', side_effect=side_effect)
+    result = rj.remove_skipped_vals(val, vals2skip)
+    assert result[0] == val[0]
 
 
 @pytest.fixture
@@ -224,54 +218,54 @@ def test_patch_and_report_w_skipped_no_patch(capsys, auth):
 
 
 def test_patch_and_report_w_patch(capsys, mocker, auth, pdict):
-    with mocker.patch('scripts.rxiv2ja.patch_metadata', return_value={'status': 'success'}):
-        result = rj.patch_and_report(auth, pdict, None, 'test_uuid', False)
-        out = capsys.readouterr()[0]
-        assert 'PATCHING - test_uuid' in out
-        for k, v in pdict.items():
-            s = '%s \t %s' % (k, v)
-            assert s in out
-        assert 'SUCCESS!' in out
-        assert result is True
+    mocker.patch('scripts.rxiv2ja.patch_metadata', return_value={'status': 'success'})
+    result = rj.patch_and_report(auth, pdict, None, 'test_uuid', False)
+    out = capsys.readouterr()[0]
+    assert 'PATCHING - test_uuid' in out
+    for k, v in pdict.items():
+        s = '%s \t %s' % (k, v)
+        assert s in out
+    assert 'SUCCESS!' in out
+    assert result is True
 
 
 def test_patch_and_report_w_fail(capsys, mocker, auth, pdict):
-    with mocker.patch('scripts.rxiv2ja.patch_metadata', return_value={'status': 'error', 'description': 'woopsie'}):
-        result = rj.patch_and_report(auth, pdict, None, 'test_uuid', False)
-        out = capsys.readouterr()[0]
-        assert 'PATCHING - test_uuid' in out
-        for k, v in pdict.items():
-            s = '%s \t %s' % (k, v)
-            assert s in out
-        assert 'FAILED TO PATCH' in out
-        assert 'woopsie' in out
-        assert not result
+    mocker.patch('scripts.rxiv2ja.patch_metadata', return_value={'status': 'error', 'description': 'woopsie'})
+    result = rj.patch_and_report(auth, pdict, None, 'test_uuid', False)
+    out = capsys.readouterr()[0]
+    assert 'PATCHING - test_uuid' in out
+    for k, v in pdict.items():
+        s = '%s \t %s' % (k, v)
+        assert s in out
+    assert 'FAILED TO PATCH' in out
+    assert 'woopsie' in out
+    assert not result
 
 
 def test_find_and_patch_item_references_no_refs(capsys, mocker, auth):
     old_uuid = 'old_uuid'
     new_uuid = 'new_uuid'
     output = "No references to %s found." % old_uuid
-    with mocker.patch('scripts.rxiv2ja.scu.get_item_ids_from_args', return_value=[]):
-        result = rj.find_and_patch_item_references(auth, old_uuid, new_uuid, False)
-        out = capsys.readouterr()[0]
-        assert output in out
-        assert result is True
+    mocker.patch('scripts.rxiv2ja.scu.get_item_ids_from_args', return_value=[])
+    result = rj.find_and_patch_item_references(auth, old_uuid, new_uuid, False)
+    out = capsys.readouterr()[0]
+    assert output in out
+    assert result is True
 
 
 def test_find_and_patch_item_references_w_refs(mocker, auth):
     old_uuid = 'old_uuid'
     new_uuid = 'new_uuid'
-    with mocker.patch('scripts.rxiv2ja.scu.get_item_ids_from_args', return_value=['bs_uuid', 'ex_uuid']):
-        with mocker.patch('scripts.rxiv2ja.patch_and_report', side_effect=[True, True]):
-            result = rj.find_and_patch_item_references(auth, old_uuid, new_uuid, False)
-            assert result is True
+    mocker.patch('scripts.rxiv2ja.scu.get_item_ids_from_args', return_value=['bs_uuid', 'ex_uuid'])
+    mocker.patch('scripts.rxiv2ja.patch_and_report', side_effect=[True, True])
+    result = rj.find_and_patch_item_references(auth, old_uuid, new_uuid, False)
+    assert result is True
 
 
 def test_find_and_patch_item_references_w_refs_one_fail(mocker, auth):
     old_uuid = 'old_uuid'
     new_uuid = 'new_uuid'
-    with mocker.patch('scripts.rxiv2ja.scu.get_item_ids_from_args', return_value=['bs_uuid', 'ex_uuid1', 'ex_uuid2']):
-        with mocker.patch('scripts.rxiv2ja.patch_and_report', side_effect=[True, False, True]):
-            result = rj.find_and_patch_item_references(auth, old_uuid, new_uuid, False)
-            assert not result
+    mocker.patch('scripts.rxiv2ja.scu.get_item_ids_from_args', return_value=['bs_uuid', 'ex_uuid1', 'ex_uuid2'])
+    mocker.patch('scripts.rxiv2ja.patch_and_report', side_effect=[True, False, True])
+    result = rj.find_and_patch_item_references(auth, old_uuid, new_uuid, False)
+    assert not result
